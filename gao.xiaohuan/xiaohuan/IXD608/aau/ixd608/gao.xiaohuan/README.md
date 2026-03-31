e24c5c87ccda76725379194db3fa9ab2ca383764
xiaohuan89 <xgao8@art.edu>
2026-03-31 01:15:43 -0700

    Delete gao.xiaohuan directory

--- gao.xiaohuan/xiaohuan/IXD608/aau/ixd608/gao.xiaohuan/README.md
+++
@@ -1,6 +0,0 @@
-# xiaohuan gao
-
-## Relevant Links
-https://xiaohuangao.com
-https://xiaohuangao.com/aau/ixd608/gao.xiaohuan
-https://xiaohuangao.com/aau/ixd608/gao.xiaohuan/styleguide
--- gao.xiaohuan/xiaohuan/IXD608/aau/ixd608/gao.xiaohuan/product card.php
+++
@@ -1,8 +0,0 @@
-<div class="product-card">
-  <div class="product-image">
-    <img src="https://placehold.co/300x220?text=Product+Image" alt="Product Name">
-  </div>
-  <h3>Product Name</h3>
-  <p class="price">$24.99</p>
-  <a href="#" class="btn secondary">View Product</a>
-</div>
